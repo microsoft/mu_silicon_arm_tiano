087a4d2b70d57c77fd2fdaaa4b3f35bd2f774d34
Chris Oo <cho@microsoft.com>
2018-09-13 20:21:34 +0000

    Merged PR 2327612: Merge changes required to build AARCH64 for Hyper-V
    Related work items: #17155713

--- ArmPkg/Include/Library/ArmLib.h
+++ ArmPkg/Include/Library/ArmLib.h
@@ -739,4 +739,16 @@ ArmGetPhysicalAddressBits (
   VOID
   );
 
+// MS_CHANGE [BEGIN] - Support Hyper-V AARCH64 with MS toolchain
+//
+// Memory Model
+//
+
+UINTN
+EFIAPI
+ArmReadIdMmfr0(
+  VOID
+  );
+// MS_CHANGE [END] - Support Hyper-V AARCH64 with MS toolchain
+
 #endif // __ARM_LIB__
