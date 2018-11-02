f735cd4481511bc5dcfc9ac4afe10fb97e3528e4
Chris Oo <cho@microsoft.com>
2018-11-02 21:58:24 +0000

    ArmPkg: GICv3: Fix ARM_GICD_IROUTER incorrect writes for SPIs
    Fix ARM_GICD_IROUTER incorrect writes, SPIs start at 32, not that the highest 32 lines aren't available.
    Fix ARM_GICD_IROUTER define to match GICv3 spec.

--- ArmPkg/Drivers/ArmGic/GicV3/ArmGicV3Dxe.c
+++ ArmPkg/Drivers/ArmGic/GicV3/ArmGicV3Dxe.c
@@ -457,7 +457,10 @@ GicV3DxeInitialize (
     }
 
     // Route the SPIs to the primary CPU. SPIs start at the INTID 32
-    for (Index = 0; Index < (mGicNumInterrupts - 32); Index++) {
+    // MU_CHANGE - SPIs per the GICv3 spec start at line 32, but the previous code
+    // relied on ARM_GICD_IROUTER to be a value different than the spec that
+    // skipped those first 32 lines.
+    for (Index = 32; Index < mGicNumInterrupts; Index++) {
       MmioWrite64 (
         mGicDistributorBase + ARM_GICD_IROUTER + (Index * 8),
         (UINT32)CpuTarget   // MU_CHANGE - ARM64 VS change
