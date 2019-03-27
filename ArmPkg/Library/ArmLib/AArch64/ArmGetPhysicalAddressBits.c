54d8030d4270cd9ba98b86603e820ea1df796a9b
Chris Oo <cho@microsoft.com>
2019-03-26 23:11:42 -0700

    Add MSFT toolchain implementation of ArmGetPhysicalAddressBits (#3) Drop missed reference when cleaning up the ArmGetPhysicalAddressBits port.

---
+++ ArmPkg/Library/ArmLib/AArch64/ArmGetPhysicalAddressBits.c
@@ -0,0 +1,34 @@
+/** @file
+
+This is a C based implementation of ArmGetPhysicalAddressBits that uses the
+already existing ArmReadIdMmfr0 with the Microsoft toolchain.
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Uefi.h>
+#include <Library/ArmLib.h>
+
+UINTN
+EFIAPI
+ArmGetPhysicalAddressBits (
+  VOID
+  )
+{
+    // Read system register ID_AA64MMFR0_EL1, valid range is bits[3:0]
+    // Valid values for ARMv8.1 PARange are 0 thru 6 which map to the corresponding address widths.
+    static UINT8 aw[7] = { 32, 36, 40, 42, 44, 48, 52 };
+    UINTN regValue = ArmReadIdMmfr0();
+    UINT8 physicalAddressWidth = 0;
+
+    // It is highly unlikely for the register to have an invalid value.
+    // Regardless, match the assembly routine and return 0.
+    if ((regValue & 0xF) < 7)
+    {
+        physicalAddressWidth = aw[regValue & 0xF];
+    }
+
+    return (UINTN) physicalAddressWidth;
+}
