9cca95fb1e7ae9401d902abd516ff13a27b8f25d
Neil Shipp <neilsh@microsoft.com>
2019-06-21 14:09:09 -0700

    Add missing stubs for new entries in PlatformBootManagerLib.h (#6)

--- ArmPkg/Library/PlatformBootManagerLib/PlatformBm.c
+++ ArmPkg/Library/PlatformBootManagerLib/PlatformBm.c
@@ -1042,6 +1042,18 @@ PlatformBootManagerWaitCallback (
 }
 
 /**
+BDS Entry  - DXE phase complete, BDS Entered.
+*/
+VOID
+EFIAPI
+PlatformBootManagerBdsEntry (
+  VOID
+  )
+{
+  return;
+}
+
+/**
   The function is called when no boot option could be launched,
   including platform recovery options and options pointing to applications
   built into firmware volumes.
@@ -1110,4 +1122,29 @@ PlatformBootManagerUnableToBoot (
   for ( ; ;) {
     EfiBootManagerBoot (&BootManagerMenu);
   }
+}
+
+/**
+ HardKeyBoot
+*/
+VOID
+EFIAPI
+PlatformBootManagerPriorityBoot (
+  UINT16  **BootNext
+  )
+{
+  return;
+}
+
+/**
+ This is called from BDS right before going into front page
+ when no bootable devices/options found
+*/
+VOID
+EFIAPI
+PlatformBootManagerProcessBootCompletion (
+  IN EFI_BOOT_MANAGER_LOAD_OPTION  *BootOption
+  )
+{
+  return;
 }
