860f28523800017a1439cec12de1481bb75a0b6f
Michael Kubacki <michael.kubacki@microsoft.com>
2020-06-17 10:49:15 -0700

    REBASE: Add ReadMe.rst and CI branches for 2008 release

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -160,12 +160,12 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
             {
                 "Path": "Common/MU_TIANO",
                 "Url": "https://github.com/Microsoft/mu_tiano_plus.git",
-                "Branch": "dev/201911_pre"
+                "Branch": "release/202008"
             },
             {
                 "Path": "MU_BASECORE",
                 "Url": "https://github.com/Microsoft/mu_basecore.git",
-                "Branch": "dev/201911_pre"
+                "Branch": "release/202008"
             }
         ]
         # MU_CHANGE END
