e3c69e74c6aab647771e24bd1d1f3a0982b653b0
Bret Barkelew <bret.barkelew@microsoft.com>
2021-04-21 14:00:42 -0700

    Sync pipelines and requirements with Basecore. Prepare release

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -47,7 +47,9 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
     # ####################################################################################### #
 
     def AddCommandLineOptions(self, parserObj):
-        pass
+        group = parserObj.add_mutually_exclusive_group()
+        group.add_argument("-force_piptools", "--fpt", dest="force_piptools", action="store_true", default=False, help="Force the system to use pip tools")
+        group.add_argument("-no_piptools", "--npt", dest="no_piptools", action="store_true", default=False, help="Force the system to not use pip tools")
 
         try:
             codeql_helpers.add_command_line_option(parserObj)
@@ -55,8 +57,11 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
             pass
 
     def RetrieveCommandLineOptions(self, args):
-        pass
-
+        # MU_CHANGE - Don't call super....lots of issues...see pytools docs...super().RetrieveCommandLineOptions(args)
+        if args.force_piptools:
+            self.UseBuiltInBaseTools = True
+        if args.no_piptools:
+            self.UseBuiltInBaseTools = False
         try:
             self.codeql = codeql_helpers.is_codeql_enabled_on_command_line(args)
         except NameError:
@@ -82,6 +87,7 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
                 "X64",
                 "ARM",
                 "AARCH64",
+                # "RISCV64"     # MU_CHANGE remove RISC-V
                 "LOONGARCH64")
 
     def GetTargetsSupported(self):
@@ -143,18 +149,37 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
 
     def GetActiveScopes(self):
         ''' return tuple containing scopes that should be active for this process '''
-        scopes = ("cibuild", "edk2-build", "host-based-test")
+        if self.ActualScopes is None:
+            scopes = ("cibuild", "edk2-build", "host-based-test")
 
-        self.ActualToolChainTag = shell_environment.GetBuildVars().GetValue("TOOL_CHAIN_TAG", "")
+            self.ActualToolChainTag = shell_environment.GetBuildVars().GetValue("TOOL_CHAIN_TAG", "")
 
-        if GetHostInfo().os.upper() == "LINUX" and self.ActualToolChainTag.upper().startswith("GCC"):
-            if "AARCH64" in self.ActualArchitectures:
-                scopes += ("gcc_aarch64_linux",)
-            if "ARM" in self.ActualArchitectures:
-                scopes += ("gcc_arm_linux",)
-            if "RISCV64" in self.ActualArchitectures:
-                scopes += ("gcc_riscv64_unknown",)
+            is_linux = GetHostInfo().os.upper() == "LINUX"
 
+            if self.UseBuiltInBaseTools is None:
+                # MU_CHANGE - redundant is_linux = GetHostInfo().os.upper() == "LINUX"
+                # try and import the pip module for basetools
+                try:
+                    import edk2basetools
+                    self.UseBuiltInBaseTools = True
+                except ImportError:
+                    self.UseBuiltInBaseTools = False
+                    pass
+
+            if self.UseBuiltInBaseTools == True:
+                scopes += ('pipbuild-unix',) if is_linux else ('pipbuild-win',)
+                logging.warning("Using Pip Tools based BaseTools")
+            else:
+                logging.warning("Falling back to using in-tree BaseTools")
+
+            if is_linux and self.ActualToolChainTag.upper().startswith("GCC"):
+                if "AARCH64" in self.ActualArchitectures:
+                    scopes += ("gcc_aarch64_linux",)
+                if "ARM" in self.ActualArchitectures:
+                    scopes += ("gcc_arm_linux",)
+                if "RISCV64" in self.ActualArchitectures:
+                    scopes += ("gcc_riscv64_unknown",)
+            self.ActualScopes = scopes
             try:
                 scopes += codeql_helpers.get_scopes(self.codeql)
 
@@ -166,7 +191,7 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
             except NameError:
                 pass
 
-        return scopes
+        return self.ActualScopes
 
     def GetRequiredSubmodules(self):
         ''' return iterable containing RequiredSubmodule objects.
@@ -198,13 +223,13 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
         return [
             {
                 "Path": "Common/MU_TIANO",
-                "Url": "https://windowspartners@dev.azure.com/windowspartners/MSCoreUEFI/_git/mu_tiano_plus",
-                "Branch": "rebase/202108_5_funsies"
+                "Url": "https://github.com/microsoft/mu_tiano_plus.git",
+                "Branch": "release/202208"
             },
             {
                 "Path": "MU_BASECORE",
-                "Url": "https://github.com/Microsoft/mu_basecore.git",
-                "Branch": "dev/201911_pre"
+                "Url": "https://github.com/microsoft/mu_basecore.git",
+                "Branch": "release/202208"
             }
         ]
         # MU_CHANGE END
