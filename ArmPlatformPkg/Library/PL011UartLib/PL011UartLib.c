4113f017d398576dfe81d260056b27da6a8a9645
Arti Gupta <argu@microsoft.com>
2022-05-06 22:38:43 +0000

    Merged PR 5288: Updated PL011UartLib.c to not wait indefinitely during read
    Updated PL011UartLib.c to not wait indefinitely during read

--- ArmPlatformPkg/Library/PL011UartLib/PL011UartLib.c
+++ ArmPlatformPkg/Library/PL011UartLib/PL011UartLib.c
@@ -405,6 +405,22 @@ PL011UartGetControl (
 }
 
 /**
+  Check to see if any data is available to be read from the debug device.
+
+  @retval TRUE       At least one byte of data is available to be read
+  @retval FALSE      No data is available to be read
+
+**/
+BOOLEAN
+EFIAPI
+PL011UartPoll (
+  IN  UINTN  UartBase
+  )
+{
+  return ((MmioRead32 (UartBase + UARTFR) & UART_RX_EMPTY_FLAG_MASK) == 0);
+}
+
+/**
   Write data to serial device.
 
   @param  Buffer           Point of data buffer which need to be written.
@@ -455,28 +471,11 @@ PL011UartRead (
 {
   UINTN  Count;
 
-  for (Count = 0; Count < NumberOfBytes; Count++, Buffer++) {
-    while ((MmioRead32 (UartBase + UARTFR) & UART_RX_EMPTY_FLAG_MASK) != 0) {
-    }
-
+  // MU_CHANGE Starts: Do not wait indefinitely for the receive buffer to get filled.
+  for (Count = 0; (Count < NumberOfBytes) && PL011UartPoll (UartBase); Count++, Buffer++) {
     *Buffer = MmioRead8 (UartBase + UARTDR);
   }
 
-  return NumberOfBytes;
-}
-
-/**
-  Check to see if any data is available to be read from the debug device.
-
-  @retval TRUE       At least one byte of data is available to be read
-  @retval FALSE      No data is available to be read
-
-**/
-BOOLEAN
-EFIAPI
-PL011UartPoll (
-  IN  UINTN  UartBase
-  )
-{
-  return ((MmioRead32 (UartBase + UARTFR) & UART_RX_EMPTY_FLAG_MASK) == 0);
+  // MU_CHANGE Ends
+  return Count;
 }
