b6aeb1b333a699baf01bd4ba103393eba53a9069
Kun Qin <kuqin@microsoft.com>
2024-11-05 15:55:08 -0800

    ArmPkg: ArmFfaLib: Various bug fixes
    This change fixed a few bugs from upstream ArmFfaLib.
    
    Signed-off-by: Kun Qin <kun.qin@microsoft.com>

--- ArmPkg/Library/ArmFfaLib/ArmFfaCommon.c
+++ ArmPkg/Library/ArmFfaLib/ArmFfaCommon.c
@@ -66,12 +66,18 @@ ConvertEfiGuidToUuid (
   UINT32  *Data32;
   UINT16  *Data16;
 
+  if ((Guid == NULL) || (Uuid == NULL)) {
+    return;
+  }
+
   CopyGuid ((EFI_GUID *)Uuid, Guid);
-  Data32    = (UINT32 *)Uuid;
-  Data32[0] = SwapBytes32 (Data32[0]);
+  Data32 = (UINT32 *)Uuid;
   Data16    = (UINT16 *)&Data32[1];
+  Data32[1] = SwapBytes32 (Data32[1]);
   Data16[0] = SwapBytes16 (Data16[0]);
   Data16[1] = SwapBytes16 (Data16[1]);
+  Data32[2] = SwapBytes32 (Data32[2]);
+  Data32[3] = SwapBytes32 (Data32[3]);
 }
 
 /**
