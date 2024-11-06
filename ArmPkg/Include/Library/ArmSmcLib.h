6c5ba4bb20af70d6a0b7973a2f1abe79fb5d2f15
Kun Qin <kuqin@microsoft.com>
2024-11-06 15:19:11 -0800

    ArmPkg: ArmSmcLib: Support all 18 registers
    This change adds a ARM SMC-ex call to support all x0-x17 registers use
    case for FF-A.
    
    Signed-off-by: Kun Qin <kun.qin@microsoft.com>

--- ArmPkg/Include/Library/ArmSmcLib.h
+++ ArmPkg/Include/Library/ArmSmcLib.h
@@ -23,6 +23,18 @@ typedef struct {
   UINTN    Arg5;
   UINTN    Arg6;
   UINTN    Arg7;
+  // MU_CHANGE Starts: Support 18 arguments for AArch64
+  UINTN    Arg8;
+  UINTN    Arg9;
+  UINTN    Arg10;
+  UINTN    Arg11;
+  UINTN    Arg12;
+  UINTN    Arg13;
+  UINTN    Arg14;
+  UINTN    Arg15;
+  UINTN    Arg16;
+  UINTN    Arg17;
+  // MU_CHANGE Ends
 } ARM_SMC_ARGS;
 
 /**
