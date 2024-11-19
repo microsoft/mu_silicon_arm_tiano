3c6e8ee4bdfdbea535514cb116224545d6b4fd68
Kun Qin <kuqin@microsoft.com>
2024-11-19 00:31:42 -0800

    ArmPkg: ArmFfaLibEx: CI Build

--- ArmPkg/ArmPkg.dsc
+++ ArmPkg/ArmPkg.dsc
@@ -141,6 +141,7 @@
   ArmPkg/Library/SemihostLib/SemihostLib.inf
   ArmPkg/Library/ArmExceptionLib/ArmExceptionLib.inf
   ArmPkg/Library/ArmExceptionLib/ArmRelocateExceptionLib.inf
+  ArmPkg/Library/ArmFfaLibEx/ArmFfaLibEx.inf
 
   ArmPkg/Drivers/CpuDxe/CpuDxe.inf
   ArmPkg/Drivers/CpuPei/CpuPei.inf
--- ArmPkg/Include/IndustryStandard/ArmFfaSvc.h
+++ ArmPkg/Include/IndustryStandard/ArmFfaSvc.h
@@ -191,7 +191,7 @@
 #define ARM_FFA_SET_MEM_ATTR_CODE_PERM_X      0
 #define ARM_FFA_SET_MEM_ATTR_CODE_PERM_XN     1
 
-#define ARM_FFA_MEM_PERM_RESERVED_MASK 0xFFFFFFF8
+#define ARM_FFA_MEM_PERM_RESERVED_MASK  0xFFFFFFF8
 
 #define ARM_FFA_SET_MEM_ATTR_MAKE_PERM_REQUEST(dataperm, codeperm)  \
     ((((codeperm) & ARM_FFA_SET_MEM_ATTR_CODE_PERM_MASK) <<         \
@@ -221,25 +221,25 @@
 #define IS_FID_FFA_ERROR(fid) \
   (fid == ARM_FID_FFA_ERROR)
 
-#define FFA_NOTIFICATIONS_FLAG_PER_VCPU (0x1 << 0)
+#define FFA_NOTIFICATIONS_FLAG_PER_VCPU  (0x1 << 0)
 
 /** Flag to delay Schedule Receiver Interrupt. */
-#define FFA_NOTIFICATIONS_FLAG_DELAY_SRI    (0x1 << 1)
+#define FFA_NOTIFICATIONS_FLAG_DELAY_SRI  (0x1 << 1)
 
-#define FFA_NOTIFICATIONS_FLAGS_VCPU_ID(id) ((id & 0xFFFF) << 16)
+#define FFA_NOTIFICATIONS_FLAGS_VCPU_ID(id)  ((id & 0xFFFF) << 16)
 
-#define FFA_NOTIFICATIONS_FLAG_BITMAP_SP    (0x1 << 0)
-#define FFA_NOTIFICATIONS_FLAG_BITMAP_VM    (0x1 << 1)
-#define FFA_NOTIFICATIONS_FLAG_BITMAP_SPM   (0x1 << 2)
-#define FFA_NOTIFICATIONS_FLAG_BITMAP_HYP   (0x1 << 3)
+#define FFA_NOTIFICATIONS_FLAG_BITMAP_SP   (0x1 << 0)
+#define FFA_NOTIFICATIONS_FLAG_BITMAP_VM   (0x1 << 1)
+#define FFA_NOTIFICATIONS_FLAG_BITMAP_SPM  (0x1 << 2)
+#define FFA_NOTIFICATIONS_FLAG_BITMAP_HYP  (0x1 << 3)
 
 /** Query interrupt ID of Notification Pending Interrupt. */
-#define FFA_FEATURE_NPI 0x1U
+#define FFA_FEATURE_NPI  0x1U
 
 /** Query interrupt ID of Schedule Receiver Interrupt. */
-#define FFA_FEATURE_SRI 0x2U
+#define FFA_FEATURE_SRI  0x2U
 
 /** Query interrupt ID of the Managed Exit Interrupt. */
-#define FFA_FEATURE_MEI 0x3U
+#define FFA_FEATURE_MEI  0x3U
 
 #endif // ARM_FFA_SVC_H_
