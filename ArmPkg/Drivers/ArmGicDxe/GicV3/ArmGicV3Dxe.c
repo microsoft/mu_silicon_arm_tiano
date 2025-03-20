8d55ad6aa6ed9e0ef3327fe42d9a84d15c0a8ed7
Vivian Nowka-Keane <vnowkakeane@microsoft.com>
2025-03-20 13:55:16 -0700

    ArmPkg: Add changes required for AARCH64 VS
    Signed-off-by: Vivian Nowka-Keane <vnowkakeane@linux.microsoft.com>

--- ArmPkg/Drivers/ArmGicDxe/GicV3/ArmGicV3Dxe.c
+++ ArmPkg/Drivers/ArmGicDxe/GicV3/ArmGicV3Dxe.c
@@ -628,7 +628,7 @@ GicV3DxeInitialize (
 
   RegValue = ArmGicV3GetControlSystemRegisterEnable ();
   if ((RegValue & ICC_SRE_EL2_SRE) == 0) {
-    ArmGicV3SetControlSystemRegisterEnable (RegValue | ICC_SRE_EL2_SRE);
+    ArmGicV3SetControlSystemRegisterEnable ((UINT32)RegValue | ICC_SRE_EL2_SRE);
     ASSERT ((ArmGicV3GetControlSystemRegisterEnable () & ICC_SRE_EL2_SRE) != 0);
   }
 
@@ -688,7 +688,7 @@ GicV3DxeInitialize (
   ArmGicV3SetPriorityMask (0xff);
 
   // Use combined priority drop and deactivate (EOImode == 0)
-  RegValue  = ArmGicV3GetControlRegister ();
+  RegValue  = (UINT64)ArmGicV3GetControlRegister ();
   RegValue &= ~(UINT64)ICC_CTLR_EOImode;
   ArmGicV3SetControlRegister (RegValue);
 
