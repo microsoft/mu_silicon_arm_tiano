63446890e106bb8ffcf48960e580165fd34e9ecf
Vivian Nowka-Keane <vnowkakeane@microsoft.com>
2025-04-21 15:46:43 -0700

    ArmPkg: Make AllocatePageTableMemory static in CpuDxe
    This fixes linker and compiler errors with ArmMmuLibPageTableAlloc.
    The MSVC errors were LNK2005, LNK1169, and Compiler #4744
    
    Signed-off-by: Vivian Nowka-Keane <vnowkakeane@linux.microsoft.com>

--- ArmPkg/Drivers/CpuDxe/PageTableMemoryAllocation.c
+++ ArmPkg/Drivers/CpuDxe/PageTableMemoryAllocation.c
@@ -104,6 +104,7 @@ FindPoolToAllocateFrom (
   @retval A pointer to the allocated page table memory or NULL if the
           allocation failed.
 **/
+STATIC
 VOID *
 AllocatePageTableMemory (
   IN UINTN  Pages
@@ -152,7 +153,7 @@ AllocatePageTableMemory (
   return Buffer;
 }
 
-PAGE_TABLE_MEM_ALLOC_PROTOCOL  mPageTableMemAllocProtocol = {
+STATIC PAGE_TABLE_MEM_ALLOC_PROTOCOL  mPageTableMemAllocProtocol = {
   AllocatePageTableMemory
 };
 
