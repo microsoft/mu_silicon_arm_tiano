947bc6503a4fcbba8f9c19a64141eedfafb72268
Kun Qin <kuqin@microsoft.com>
2025-09-09 01:04:49 -0700

    MU_CHANGE taf

--- ArmPkg/Drivers/ArmPsciMpServicesDxe/ArmPsciMpServicesDxe.c
+++ ArmPkg/Drivers/ArmPsciMpServicesDxe/ArmPsciMpServicesDxe.c
@@ -887,8 +887,10 @@ EnableDisableAP (
     return EFI_INVALID_PARAMETER;
   }
 
+  // MU_CHANGE Starts: Fix for invocation on timer expired APs.
   if ((GetApState (CpuData) != CpuStateIdle) &&
       (GetApState (CpuData) != CpuStateFinished)) {
+  // MU_CHANGE Ends
     return EFI_UNSUPPORTED;
   }
 
